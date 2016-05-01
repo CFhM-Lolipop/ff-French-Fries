28c6aed85c328e04f230910d62e35ba2609087be
CFhM-Lolipop <873598708@bit.edu.cn>
2016-05-01 16:35:08 +0800

    first upload

---
+++ frenchFries/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frenchFries
+{
+    class Program
+    {
+        private static bool canExit = false;
+        static void Main(string[] args)
+        {
+            string inputName = "", inputMount = "";
+            people p = new people();
+            Console.Write("please input who is going to eat French Fries:");
+            inputName = Console.ReadLine();
+            Console.Write("please input how many French Fries {0} wants to eat:", inputName);
+            inputMount = Console.ReadLine();
+            int num = int.Parse(inputMount);
+            p.name = inputName;
+            p.eating(num);
+            p.f.nothingToEat += F_nothingToEat;
+            while(!canExit)
+            {
+                ;
+            }
+        }
+
+        private static void F_nothingToEat()
+        {
+            canExit = true;
+        }
+    }
+}
---
+++ frenchFries/freanFries.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace frenchFries
+{
+    delegate void eventHandler();
+    class frenchFries
+    {
+        private int _mountOfFrenchFries;
+        public event eventHandler eatFrenchFries;
+        public event eventHandler nothingToEat;
+        private Timer t = null;
+        public int mount
+        {
+            get
+            {
+                return _mountOfFrenchFries;
+            }
+            set
+            {
+                _mountOfFrenchFries = value;
+            }
+        }
+
+        public void beginEating()
+        {
+            t = new Timer(eat, null, 0, 1000);
+        }
+
+        public void eat(object o)
+        {
+            _mountOfFrenchFries -= 2;
+            if (eatFrenchFries != null)
+            {
+                eatFrenchFries();
+            }
+
+            if (_mountOfFrenchFries <= 0)
+            {
+                if (nothingToEat != null)
+                {
+                    nothingToEat();
+                }
+
+                t.Dispose();
+                t = null;
+            }
+        }
+    }
+
+    class people
+    {
+        private string _name = "";
+        private int cnt = 0;
+        public frenchFries f = new frenchFries();
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public void eating(int mount)
+        {
+            f.mount = mount;
+            cnt = mount;
+            f.eatFrenchFries += F_eatFrenchFries;
+            f.nothingToEat += F_nothingToEat;
+            f.beginEating();
+        }
+
+        private void F_nothingToEat()
+        {
+            Console.WriteLine("poor {0} has nothing to eat", name);
+        }
+
+        private void F_eatFrenchFries()
+        {
+            Console.Write(string.Format("{0, 3}%\r",f.mount*100/cnt));
+        }
+    }
+}
